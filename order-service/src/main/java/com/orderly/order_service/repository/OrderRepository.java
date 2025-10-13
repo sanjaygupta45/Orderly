0d2d78c7f1fe549be87bcb04d408648844d0b48e
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2025-10-13 20:05:52 +0530

    added order service

---
+++ order-service/src/main/java/com/orderly/order_service/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.orderly.order_service.repository;
+
+import com.orderly.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
